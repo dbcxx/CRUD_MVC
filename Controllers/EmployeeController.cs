3aa753815c867ed9320513d6cd7e2f89920884fa
dbcxx <40644301+dbcxx@users.noreply.github.com>
2020-11-05 14:05:16 +0100

    added a list view

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace CRUD_mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+
+            return View(employees);
+        }
+    }
+}
