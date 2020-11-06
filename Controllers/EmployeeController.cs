8a29f75d46777b7eb57f4632abf68782e30a72cc
dbcxx <40644301+dbcxx@users.noreply.github.com>
2020-11-06 14:24:10 +0100

    sets an object binding model to insert ti SQL

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -9,13 +9,39 @@ namespace CRUD_mvc.Controllers
 {
     public class EmployeeController : Controller
     {
-        // GET: Employee
+        
         public ActionResult Index()
         {
             EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
             List<Employee> employees = employeeBusinessLayer.Employees.ToList();
 
             return View(employees);
+        }
+
+
+        [HttpGet]
+        [ActionName("Create")]
+        public ActionResult Create_Get()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create_Post()
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = new Employee();
+                UpdateModel<Employee>(employee);
+
+                EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+                employeeBusinessLayer.AddEmployee(employee);
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
